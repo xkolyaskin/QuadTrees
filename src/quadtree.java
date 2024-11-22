0a3dbd6a8d137225fec52faae639f3edd749c2d6
Alex Kolyaskin <akolyask@purdue.edu>
2024-11-22 17:17:15 -0500

    implemented subdivide() in quadtree.java. added todo for command line interface in main()

--- src/quadtree.java
+++ src/quadtree.java
@@ -21,6 +21,28 @@ public class quadtree {
      */
     public void subdivide(quadtreeNode root) {
         // todo implement subdivide
+        int xmin = root.region.xmin;
+        int ymin = root.region.ymin;
+        int xmax = root.region.xmax;
+        int ymax = root.region.ymax;
+
+        int xmid = (xmin + xmax) / 2;
+        int ymid = (ymin + ymax) / 2;
+
+        root.children.add(new quadtreeNode(new region(xmin, ymid, xmid, ymax))); // NW
+        root.children.add(new quadtreeNode(new region(xmid, ymid, xmax, ymax))); // NE
+        root.children.add(new quadtreeNode(new region(xmin, ymin, xmid, ymid))); // SW
+        root.children.add(new quadtreeNode(new region(xmid, ymin, xmax, ymid))); // SE
+
+        if (!(root.point == null)) {
+            point P = root.point;
+            root.point = null;
+            for (quadtreeNode quadrant : root.children) {
+                if (insert(P, quadrant)) {
+                    return;
+                }
+            }
+        }
     }
 
     /**
@@ -48,5 +70,14 @@ public class quadtree {
             }
         }
         return false;
+    }
+
+    /**
+     * Main function for testing
+     *
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+        // todo implement command line interface
     }
 }
